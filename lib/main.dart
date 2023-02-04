a273f1fd15ee8660cb6a4d235004ecdc1bc6feae
japarico <japa_rico@yahoo.com>
2023-02-04 17:50:40 +0800

    commited.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
-
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:infinite_listview/infinite_listview.dart';
 void main() {
