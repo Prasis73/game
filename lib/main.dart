70e951aa31538b99ebea424da80424bb424bd9c8
japarico <japa_rico@yahoo.com>
2023-02-07 13:19:00 +0800

    commited.

--- lib/main.dart
+++ lib/main.dart
@@ -1,94 +1,140 @@
 import 'dart:async';
-import 'package:carousel_slider/carousel_slider.dart';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
-import 'package:infinite_listview/infinite_listview.dart';
+import 'package:flutter_fortune_wheel/flutter_fortune_wheel.dart';
+import 'package:roulette/roulette.dart';
+import 'arrow.dart';
+
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Roulette',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class MyRoulette extends StatelessWidget {
+  const MyRoulette({
+    Key? key,
+    required this.controller,
+  }) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  final RouletteController controller;
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
-  final String title;
+
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    StreamController<int> controller = StreamController<int>();
+    return Stack(
+      alignment: Alignment.topCenter,
+      children: [
+        SizedBox(
+          width: 260,
+          height: 260,
+          child: Padding(
+            padding: const EdgeInsets.only(top: 30),
+            child: FortuneBar(
+              selected: controller.stream,
+              styleStrategy: const AlternatingStyleStrategy(),
+              items: const [
+                FortuneItem(child: Text('Han Solo'),
+            style: FortuneItemStyle(
+            color: Colors.red, // <-- custom circle slice fill color
+            borderColor: Colors.green, // <-- custom circle slice stroke color
+            borderWidth: 3, // <-- custom circle slice stroke width
+          ),),
+                FortuneItem(child: Text('Yoda')),
+                FortuneItem(child: Text('Obi-Wan Kenobi')),
+              ],
+            )
+          ),
+        ),
+        const Arrow(),
+      ],
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  final InfiniteScrollController _infiniteController = InfiniteScrollController(
-    initialScrollOffset: 0.0,
-  );
-  StreamController<int> controller = StreamController<int>();
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+
+
+  late RouletteController _controller;
+
+  final colors = <Color>[
+    Colors.red,
+    Colors.grey,
+    Colors.red,
+    Colors.grey,
+    Colors.red,
+    Colors.grey,
+    Colors.red,
+    Colors.grey,
+    Colors.white,
+
+  ];
+
+  @override
+  void initState() {
+    // Initialize the controller
+    final group = RouletteGroup.uniform(
+      colors.length,
+      colorBuilder: colors.elementAt,
+      textBuilder: (index) => (index + 1).toString(),
+    );
+    _controller = RouletteController(vsync: this, group: group);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        // Leave margin here for top bar
-        color: Colors.grey[900],
-        padding: EdgeInsets.fromLTRB(10.0, 20.0, 10.0, 5.0),
-        child: (Column(mainAxisSize: MainAxisSize.min, children: [
-          Expanded(
-
-            child: InfiniteListView.builder(
-              // itemCount: 222,
-              scrollDirection: Axis.horizontal,
-              controller: _infiniteController,
-              anchor: 0.5,
-              itemBuilder: (BuildContext context, int index) {
-                return Material(
-                  child: InkWell(
-                    onTap: () {},
-                    child: SizedBox(
-                      width: 100,
-                      child: ListTile(
-                        title: Text('Item #$index'),
-                        subtitle: Text('Subtitle $index'),
-                        trailing: const Icon(Icons.chevron_right),
-                      ),
-                    ),
-                  ),
-                );
-              },
-            ),
-          ),
-        ])),
+      appBar: AppBar(
+        title: const Text('Roulette'),
       ),
+      body: Container(
+        decoration: BoxDecoration(
+          color: Colors.pink.withOpacity(0.1),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              MyRoulette(controller: _controller),
+            ],
+          ),
+        ),
+      ),
+
     );
   }
-}
 
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+}
